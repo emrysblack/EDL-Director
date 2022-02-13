c2a4ffa88e4d97de96e983a49b41f35da710f86e
Emrys Black <emrysblack@gmail.com>
2022-02-12 20:22:05 -0600

    changed settings to default with override model

--- settings.js
+++ settings.js
@@ -2,14 +2,18 @@ const fs = require("fs");
 const ini = require("ini");
 const path = require("path");
 
+const mainIni = path.join(__dirname, "main.ini");
 const defaultIni = path.join(__dirname, "default.ini");
-const userIni = path.join(__dirname, "user.ini");
+const settingsIni = "settings.ini";
 
 try {
-  fs.copyFileSync(defaultIni, userIni, fs.constants.COPYFILE_EXCL);
+  fs.copyFileSync(defaultIni, settingsIni, fs.constants.COPYFILE_EXCL);
 } catch (error) {
-  console.log(`File exists: ${userIni}`);
+  console.log(`File exists: ${settingsIni}`);
 }
 
-const config = ini.parse(fs.readFileSync(userIni, "utf-8"));
+const config = {
+  ...ini.parse(fs.readFileSync(mainIni, "utf-8")),
+  ...ini.parse(fs.readFileSync(settingsIni, "utf-8")),
+};
 module.exports = config;
